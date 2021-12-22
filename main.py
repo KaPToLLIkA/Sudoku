3885330c91f4f597e333122632e5983bb6f8ce46
hexagony <eduardpur18@gmail.com>
2021-12-23 00:45:13 +0300

    final no serialization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 import sys
-import os
 import random
 
 
@@ -47,6 +46,7 @@ class Game:
         self.wrong = 0
         self.zeros = count_zeros(self.unsolved)
         self.duplicate_pos = [0, 0]
+        self.git = Git(self)
 
     def __check_segment(self, ix, iy, value: int):
         sx = ix // 3
@@ -109,20 +109,28 @@ class Game:
         y = int_args[1] - 1
         value = int_args[2]
 
+        if value < 1 or value > 9:
+            print(f'Value must be in range [1; 9]')
+            return
+
         if 0 <= x <= 8 and 0 <= y <= 8:
             if not self.__check_segment(x, y, value) \
                     and not self.__check_row(y, value) \
                     and not self.__check_column(x, value):
-                if self.unsolved[y][x] == 0:
+                if self.unsolved[y][x] != value:
                     print(f'Set value at x:{x + 1} y:{y + 1}')
+                    if self.unsolved[y][x] == 0:
+                        self.zeros -= 1
+                        self.git.index.stage_zero(-1)
                     self.unsolved[y][x] = value
-                    self.zeros -= 1
+                    self.git.index.stage_insert([x, y, value])
                 else:
                     print(f'No effect')
             else:
                 print('Wrong answer')
                 print(f'Duplicate at x:{self.duplicate_pos[0] + 1} y:{self.duplicate_pos[1] + 1}')
                 self.wrong += 1
+                self.git.index.stage_wrong(1)
         else:
             print('Wrong indexes')
 
@@ -144,10 +152,185 @@ class Game:
             if self.unsolved[y][x] != 0:
                 self.unsolved[y][x] = 0
                 self.zeros += 1
+                self.git.index.stage_delete([x, y, 0])
+                self.git.index.stage_zero(1)
             else:
                 print(f'No effect')
         else:
             print('Wrong indexes')
+
+    def cmd_log(self, args):
+        self.git.log(args)
+
+    def cmd_redo(self, args):
+        self.git.redo(args)
+
+    def cmd_undo(self, args):
+        self.git.undo(args)
+
+    def cmd_commit(self, args):
+        self.git.commit(args[0])
+
+    def save(self):
+        with open(self.file_name, 'w') as f:
+            for row in self.unsolved:
+                f.write(f'{",".join(list(map(lambda x: str(x), row)))}\n')
+            f.write(f'{self.zeros},{self.wrong}\n')
+
+    def load(self, file_name):
+        self.file_name = file_name
+        try:
+            with open(file_name, 'r') as f:
+                self.unsolved = []
+                for i in range(0, 9):
+                    self.unsolved.append(list(map(lambda x: int(x), f.readline().split(','))))
+                other = list(map(lambda x: int(x), f.readline().split(',')))
+                self.zeros = other[0]
+                self.wrong = other[1]
+        except FileNotFoundError:
+            pass
+
+
+def safe_get_int_in_range(min_inclusive: int, max_exclusive: int):
+    while True:
+        try:
+            value = int(input())
+            if min_inclusive <= value < max_exclusive:
+                return value
+            else:
+                print(f'Value not in range [{min_inclusive};{max_exclusive})')
+        except ValueError:
+            print(f'Value must be int')
+
+
+class Commit:
+    def __init__(self, p_id, name: str):
+        self.name = name
+        self.inserted = []
+        self.deleted = []
+        self.d_wrong = 0
+        self.d_zero = 0
+        self.p_id = p_id
+        self.c_ids = []
+
+    def stage_insert(self, insert):
+        for rem in self.deleted:
+            if rem[0] == insert[0] and rem[1] == insert[1]:
+                self.deleted.remove(rem)
+        for ins in self.inserted:
+            if set(ins) == set(insert):
+                return
+        for ins in self.inserted:
+            if ins[0] == insert[0] and ins[1] == insert[1]:
+                ins[2] = insert[2]
+                return
+        self.inserted.append(insert)
+
+    def stage_delete(self, delete):
+        for ins in self.inserted:
+            if ins[0] == delete[0] and ins[1] == delete[1]:
+                self.inserted.remove(ins)
+        for rem in self.deleted:
+            if set(rem) == set(delete):
+                return
+        for rem in self.deleted:
+            if rem[0] == delete[0] and rem[1] == delete[1]:
+                rem[2] = delete[2]
+                return
+        self.deleted.append(delete)
+
+    def stage_wrong(self, wrong):
+        self.d_wrong += wrong
+
+    def stage_zero(self, zero):
+        self.d_zero += zero
+
+    def __str__(self):
+        return f'Parent: {self.p_id:5} DWrong: {self.d_wrong: 5} DZeros: {self.d_zero:5} ' \
+               f'Inserted: {self.inserted} Removed: {self.deleted}'
+
+
+class Git:
+    def __init__(self, game: Game):
+        self.commits = [Commit(-1, 'init')]
+        self.index = Commit(-1, 'index')
+        self.head = 0
+        self.game = game
+        self.last_state_file = 'state.dt'
+        self.git = 'git.dt'
+        self.game.load(self.last_state_file)
+
+    def revert_index(self):
+        # undo index changes
+        for ins in self.index.inserted:
+            self.game.unsolved[ins[0]][ins[1]] = 0
+        for rem in self.index.deleted:
+            self.game.unsolved[rem[0]][rem[1]] = rem[2]
+        self.game.wrong -= self.index.d_wrong
+        self.game.zeros -= self.index.d_zero
+        self.index = Commit(-1, 'index')
+
+    def undo(self, args):
+        cur_commit = self.commits[self.head]
+
+        if cur_commit.p_id == -1:
+            print(f'Root of tree... head_id {self.head} p_id {cur_commit.p_id}')
+            return
+
+        self.revert_index()
+
+        for ins in cur_commit.inserted:
+            self.game.unsolved[ins[0]][ins[1]] = 0
+        for rem in cur_commit.deleted:
+            self.game.unsolved[rem[0]][rem[1]] = rem[2]
+        self.game.wrong -= cur_commit.d_wrong
+        self.game.zeros -= cur_commit.d_zero
+
+        self.head = self.commits[self.head].p_id
+
+    def log(self, args):
+        for commit in self.commits:
+            print(f'[{self.commits.index(commit)}] {commit}')
+        print(f'HEAD: {self.head}\n'
+              f'INDEX:\n====\n{self.index}\n'
+              f'====')
+
+    def redo(self, args):
+        count = len(self.commits[self.head].c_ids)
+        if count > 0:
+            if count == 1:
+                next_head = self.commits[self.head].c_ids[0]
+                next_commit = self.commits[next_head]
+            else:
+                print(f'You should choose commit... print index to select:')
+                for i in range(0, count):
+                    print(f'[{i}] commit name: {self.commits[self.commits[self.head].c_ids[i]].name}')
+                next_id = safe_get_int_in_range(0, count)
+                next_head = self.commits[self.head].c_ids[next_id]
+                next_commit = self.commits[next_head]
+
+            self.revert_index()
+
+            # apply commit changes
+            for ins in next_commit.inserted:
+                self.game.unsolved[ins[0]][ins[1]] = ins[2]
+            for rem in next_commit.deleted:
+                self.game.unsolved[rem[0]][rem[1]] = 0
+            self.game.wrong += next_commit.d_wrong
+            self.game.zeros += next_commit.d_zero
+
+            self.head = next_head
+        else:
+            print(f'End of tree... head_id {self.head} leaf_count: {count}')
+
+    def commit(self, name):
+        self.index.p_id = self.head
+        self.index.name = name
+        self.commits[self.head].c_ids.append(len(self.commits))
+        self.commits.append(self.index)
+        self.head = len(self.commits) - 1
+        self.index = Commit(-1, 'index')
+        print(f'Commited:\n====\n{self.commits[self.head]}\n====')
 
 
 class Window:
@@ -174,6 +357,10 @@ class Window:
             's': self.game.cmd_set_value_at,
             'd': self.game.cmd_del_value_at,
             'e': self.cmd_exit,
+            'c': self.game.cmd_commit,
+            'u': self.game.cmd_undo,
+            'r': self.game.cmd_redo,
+            'l': self.game.cmd_log,
         }
 
         for key, value in commands.items():
@@ -181,6 +368,7 @@ class Window:
                 value(args)
 
     def cmd_exit(self, args):
+        self.game.save()
         sys.exit(0)
 
 
